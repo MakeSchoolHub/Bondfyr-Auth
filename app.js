5f0275b7926cdb0ff6cc4c550f8eece3a90f1b3d
Muhammad Tifak <moht20009@gmail.com>
2020-10-01 12:34:56 -0400

    Template engine + index routes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const connectDB = require('./config/db')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db') 
 
 //Loading config
 dotenv.config({ path: './config/config.env' })
@@ -10,9 +11,17 @@ connectDB()
 
 const app = express()
 
+//Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+//Handlebars...heck yeah
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', '.hbs')
+
+//Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
